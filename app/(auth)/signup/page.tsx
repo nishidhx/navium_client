648722873b905cb4ccdf80645cdbd4bbe3050b4d
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-25 05:33:27 +0530

    auth pagess init

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,6 @@
+import { AuthPage } from "@/components/auth";
+
+
+export default function SignIn() {
+    return <AuthPage type={"1"} />
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/utils/clsx";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +18,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("no-scrollbar", "h-full", "antialiased", "font-sans", geist.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="font-hanken-grotesk min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
